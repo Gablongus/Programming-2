8a4000c407166d3e7f0c3864bc21e0b24b4b5a7c
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2025-02-17 15:24:23 -0600

    2/17/25

---
+++ Pg272LongDistance/Pg272LongDistance/MainForm.py
@@ -0,0 +1,192 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._radioButton1 = System.Windows.Forms.RadioButton()
+        self._radioButton2 = System.Windows.Forms.RadioButton()
+        self._radioButton3 = System.Windows.Forms.RadioButton()
+        self._textBox1 = System.Windows.Forms.TextBox()
+        self._label1 = System.Windows.Forms.Label()
+        self._label2 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self._label3 = System.Windows.Forms.Label()
+        self._label4 = System.Windows.Forms.Label()
+        self.SuspendLayout()
+        # 
+        # radioButton1
+        # 
+        self._radioButton1.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._radioButton1.Location = System.Drawing.Point(25, 29)
+        self._radioButton1.Name = "radioButton1"
+        self._radioButton1.Size = System.Drawing.Size(154, 37)
+        self._radioButton1.TabIndex = 0
+        self._radioButton1.TabStop = True
+        self._radioButton1.Text = "Daytime"
+        self._radioButton1.UseVisualStyleBackColor = True
+        self._radioButton1.CheckedChanged += self.RadioButton1CheckedChanged
+        # 
+        # radioButton2
+        # 
+        self._radioButton2.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._radioButton2.Location = System.Drawing.Point(25, 62)
+        self._radioButton2.Name = "radioButton2"
+        self._radioButton2.Size = System.Drawing.Size(154, 37)
+        self._radioButton2.TabIndex = 1
+        self._radioButton2.TabStop = True
+        self._radioButton2.Text = "Evening"
+        self._radioButton2.UseVisualStyleBackColor = True
+        self._radioButton2.CheckedChanged += self.RadioButton2CheckedChanged
+        # 
+        # radioButton3
+        # 
+        self._radioButton3.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._radioButton3.Location = System.Drawing.Point(25, 93)
+        self._radioButton3.Name = "radioButton3"
+        self._radioButton3.Size = System.Drawing.Size(154, 37)
+        self._radioButton3.TabIndex = 2
+        self._radioButton3.TabStop = True
+        self._radioButton3.Text = "Off-Peak"
+        self._radioButton3.UseVisualStyleBackColor = True
+        self._radioButton3.CheckedChanged += self.RadioButton3CheckedChanged
+        # 
+        # textBox1
+        # 
+        self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._textBox1.Location = System.Drawing.Point(144, 40)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(100, 26)
+        self._textBox1.TabIndex = 3
+        # 
+        # label1
+        # 
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(144, 9)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(100, 23)
+        self._label1.TabIndex = 4
+        self._label1.Text = "Minutes:"
+        # 
+        # label2
+        # 
+        self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label2.Location = System.Drawing.Point(38, 9)
+        self._label2.Name = "label2"
+        self._label2.Size = System.Drawing.Size(100, 23)
+        self._label2.TabIndex = 5
+        self._label2.Text = "Category:"
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.Thistle
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(12, 191)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(118, 60)
+        self._button1.TabIndex = 6
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.Thistle
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(136, 191)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(118, 60)
+        self._button2.TabIndex = 7
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.Thistle
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(76, 257)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(118, 60)
+        self._button3.TabIndex = 8
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # label3
+        # 
+        self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label3.Location = System.Drawing.Point(13, 156)
+        self._label3.Name = "label3"
+        self._label3.Size = System.Drawing.Size(100, 23)
+        self._label3.TabIndex = 9
+        self._label3.Text = "Charge:"
+        # 
+        # label4
+        # 
+        self._label4.BackColor = System.Drawing.SystemColors.ControlLightLight
+        self._label4.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label4.Location = System.Drawing.Point(94, 151)
+        self._label4.Name = "label4"
+        self._label4.Size = System.Drawing.Size(150, 30)
+        self._label4.TabIndex = 10
+        self._label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.Lavender
+        self.ClientSize = System.Drawing.Size(275, 339)
+        self.Controls.Add(self._label4)
+        self.Controls.Add(self._label3)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._label2)
+        self.Controls.Add(self._label1)
+        self.Controls.Add(self._textBox1)
+        self.Controls.Add(self._radioButton3)
+        self.Controls.Add(self._radioButton2)
+        self.Controls.Add(self._radioButton1)
+        self.Name = "MainForm"
+        self.Text = "Pg272LongDistance"
+        self.ResumeLayout(False)
+        self.PerformLayout()
+
+
+   
+
+   
+        
+    def RadioButton1CheckedChanged(self, sender, e):
+        rate = 0.07
+        
+    
+    def RadioButton2CheckedChanged(self, sender, e):
+        rate = 0.12
+        
+       
+    def RadioButton3CheckedChanged(self, sender, e):
+        rate = 0.05
+        
+        
+    
+
+    def Button1Click(self, sender, e):
+        min = self._textBox1.Text
+        cost = min * rate
+        self._label4.Text = str(cost)
+        
+
+    def Button2Click(self, sender, e):
+        self._textBox1.Text = ""
+        self._label4.Text = ""
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
---
+++ StrInt3/StrInt3/MainForm.py
@@ -0,0 +1,66 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._label1 = System.Windows.Forms.Label()
+        self._textBox1 = System.Windows.Forms.TextBox()
+        self._label2 = System.Windows.Forms.Label()
+        self._label3 = System.Windows.Forms.Label()
+        self.SuspendLayout()
+        # 
+        # label1
+        # 
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(37, 43)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(110, 23)
+        self._label1.TabIndex = 0
+        self._label1.Text = "Input String:"
+        # 
+        # textBox1
+        # 
+        self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._textBox1.Location = System.Drawing.Point(153, 40)
+        self._textBox1.Name = "textBox1"
+        self._textBox1.Size = System.Drawing.Size(100, 26)
+        self._textBox1.TabIndex = 1
+        # 
+        # label2
+        # 
+        self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._label2.Location = System.Drawing.Point(12, 87)
+        self._label2.Name = "label2"
+        self._label2.Size = System.Drawing.Size(257, 23)
+        self._label2.TabIndex = 2
+        self._label2.Text = "First Non Repeating Char:"
+        # 
+        # label3
+        # 
+        self._label3.BackColor = System.Drawing.SystemColors.ControlLightLight
+        self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label3.Location = System.Drawing.Point(240, 87)
+        self._label3.Name = "label3"
+        self._label3.Size = System.Drawing.Size(146, 31)
+        self._label3.TabIndex = 3
+        self._label3.Text = "label3"
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.Azure
+        self.ClientSize = System.Drawing.Size(499, 383)
+        self.Controls.Add(self._label3)
+        self.Controls.Add(self._label2)
+        self.Controls.Add(self._textBox1)
+        self.Controls.Add(self._label1)
+        self.Name = "MainForm"
+        self.Text = "StrInt3"
+        self.ResumeLayout(False)
+        self.PerformLayout()
+
