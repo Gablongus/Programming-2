f893f6ca56382fe6baa574337bdb4d572f92edcd
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2025-02-19 15:24:45 -0600

    2/19/25

---
+++ Pg435TicketSales/Pg435TicketSales/MainForm.py
@@ -0,0 +1,95 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._label1 = System.Windows.Forms.Label()
+        self._label2 = System.Windows.Forms.Label()
+        self._label3 = System.Windows.Forms.Label()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # label1
+        # 
+        self._label1.BackColor = System.Drawing.SystemColors.ButtonFace
+        self._label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label1.Location = System.Drawing.Point(13, 9)
+        self._label1.Name = "label1"
+        self._label1.Size = System.Drawing.Size(402, 216)
+        self._label1.TabIndex = 1
+        self._label1.Text = "Select the Type of Ticket Sales"
+        # 
+        # label2
+        # 
+        self._label2.BackColor = System.Drawing.SystemColors.ButtonFace
+        self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label2.Location = System.Drawing.Point(31, 50)
+        self._label2.Name = "label2"
+        self._label2.Size = System.Drawing.Size(186, 60)
+        self._label2.TabIndex = 2
+        self._label2.Text = "Select to see all sales for general public:"
+        # 
+        # label3
+        # 
+        self._label3.BackColor = System.Drawing.SystemColors.ButtonFace
+        self._label3.Font = System.Drawing.Font("Microsoft Sans Serif", 9.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._label3.Location = System.Drawing.Point(31, 130)
+        self._label3.Name = "label3"
+        self._label3.Size = System.Drawing.Size(186, 60)
+        self._label3.TabIndex = 3
+        self._label3.Text = "Select so see all student ticket sales:"
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.SystemColors.Control
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 8.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(223, 50)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(132, 47)
+        self._button1.TabIndex = 4
+        self._button1.Text = "General Sales"
+        self._button1.UseVisualStyleBackColor = False
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.SystemColors.Control
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 8.25, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(223, 130)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(132, 47)
+        self._button2.TabIndex = 5
+        self._button2.Text = "Student Sales"
+        self._button2.UseVisualStyleBackColor = False
+        # 
+        # button3
+        # 
+        self._button3.Location = System.Drawing.Point(316, 242)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(99, 32)
+        self._button3.TabIndex = 6
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = True
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.SystemColors.ActiveCaption
+        self.ClientSize = System.Drawing.Size(433, 295)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._label3)
+        self.Controls.Add(self._label2)
+        self.Controls.Add(self._label1)
+        self.Name = "MainForm"
+        self.Text = "Pg435TicketSales"
+        self.ResumeLayout(False)
+
