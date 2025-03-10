9d36558fcfa06197b698c88de00de99715a0fe2b
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2025-03-10 15:25:10 -0500

    2/20/25

--- prog285b.py
+++ prog285b.py
@@ -11,6 +11,12 @@ def main():
                 code = int(data[1])
                 sales = float(data[2])
 
+                # Option 2: List Comprehension
+                # id, code, sales = [float(x) for x in line.split(" ")]
+                # Option 3: Conditional List Comprehension
+                # id, code, sales = [float(x) if '.' in x else int(x) for x in line.split(" ")]
+
+
                 person = Salesperson(id, code, sales)
                 person.calc_commission()
                 print(str(person))  # print(person)
