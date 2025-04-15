35e9abc57de570005e15d01d61224feb7f3a89b9
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2025-04-15 15:23:58 -0500

    4/15/25

---
+++ Program88a/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: schraedl.g
+ * Date: 4/15/2025
+ * Time: 2:46 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Program88a
+{
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+            
+            //
+            // TODO: Add constructor code after the InitializeComponent() call.
+            //
+        }
+    }
+}
---
+++ Program88a/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: schraedl.g
+ * Date: 4/15/2025
+ * Time: 2:56 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Program88a
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter num1: ");
+            int num1 = int.Parse(Console.Readline());
+            Console.Write("Enter num2: ");
+            interface num2 = int.Parse(Console.ReadLine());
+            
+            int sum = num1 + num2;
+            int dif = num2 - num2;
+            int prd = num1 * num2;
+            double avg = (double)sum / 2.0;
+            int abs = Math.Abs(dif);
+            // Math.Max and Math.Min
+            int max, min;
+            
+            if (num1 > num2) {
+                max = num1;
+            } else {
+                max = num2;
+            }
+            
+            if (num1 <= num2)
+                min = num1;
+            else min = num2;
+            // else if
+            
+            Console.WriteLine("Sum: " + sum);
+            // TODO: the rest
+            Console.WriteLine("Max: " + max);
+            Console.WriteLine("Min: " + min);
+            Console.ReadLine();
+        }
+    }
+}
