e99d2bf86672f362ad83bf03b467920019e673d4
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2025-04-22 15:24:42 -0500

    4/22/25
    i have no idea what im doing anymore

---
+++ LP4-1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter # of copies to print: ");
+int copies = int.Parse(Console.ReadLine());
+double price = 0;
+double cost = 0;
+// && AND, || OR, ! NOT
+if (copies > 0 && copies <= 99)          price = 0.30;
+else if (copies > 99 && copies <= 499)   price = 0.28;
+else if (copies > 499 && copies <= 749)  price = 0.27;
+else if (copies > 750 && copies <= 1000) price = 0.26;
+else if (copies > 1000)                  price = 0.25;
+else Console.WriteLine("Invalid number of copies!");
+
+cost = price * copies;
+Console.WriteLine("Total Price: " + copies)
---
+++ Lang54c Console/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: schraedl.g
+ * Date: 4/22/2025
+ * Time: 3:22 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Lang54c_Console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
