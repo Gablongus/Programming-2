6af2ec3404a707f411d2e0932c31036c1510ce4c
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2025-04-23 15:24:05 -0500

    4/23/25
    I have a slightly better idea of what I am doing.

---
+++ CSharp/Lang54d Console/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: schraedl.g
+ * Date: 4/23/2025
+ * Time: 3:10 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Lang54d_Console
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Base:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Height:");
+            double h = double.Parse(Console.ReadLine());
+            
+            double hyp = Math.Round(Math.Sqrt((b * b) + (h * h)), 2);
+            Console.WriteLine("Hypotenuse = "+ hyp);
+            Console.ReadLine();
+                
+          
+        }
+    }
+}
--- Lang54c Console/Program.cs
+++ Lang54c Console/Program.cs
@@ -14,10 +14,18 @@ namespace Lang54c_Console
     {
         public static void Main(string[] args)
         {
-           
+            Console.WriteLine("Enter Radius");
+            double radius = double.Parse(Console.ReadLine());
+            double Pi = 3.14159;
+            double Diameter =  radius * 2;
+            double Area = Math.Round(Pi * (radius * radius),3);
+            double Circum = Math.Round(2 * Pi * radius, 3);
+            Console.WriteLine("Diameter = " + Diameter);
+            Console.WriteLine("Area = " + Area);
+            Console.WriteLine("Circumference = " + Circum);
             
-            Console.Write("Press any key to continue . . . ");
-            Console.ReadKey(true);
+            Console.ReadLine();
+            
         }
     }
 }
