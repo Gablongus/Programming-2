683662ce80a13fbd70d18d607d4bdec84c31a6c2
Gablongus <schraedl.g@stu.janesville.k12.wi.us>
2025-05-19 15:23:55 -0500

    5/19/25

---
+++ CSharp/StrInterview18/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: schraedl.g
+ * Date: 5/19/2025
+ * Time: 3:06 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace StrInterview18
+{
+    class Program
+    {
+ 
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A String");
+            string ogstr = Console.ReadLine();
+            string str = ogstr;
+            Console.WriteLine("Enter Charecter to be removed");
+            string cha = (Console.ReadLine());
+            str = str.Replace(cha, "");
+            Console.WriteLine(ogstr + " without cha " + cha + " is: " + str);
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/StrInterview8/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: schraedl.g
+ * Date: 5/19/2025
+ * Time: 2:35 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace StrInterview8
+{
+    class Program
+    {
+        static int Count(string stringaling, char find)
+        {
+            return stringaling.Split(find).Length - 1;
+        }
+        
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String");
+            string str = Console.ReadLine();
+            Console.WriteLine("Enter Charecter to be found");
+            char cha = Convert.ToChar(Console.ReadLine());
+            int num = Count(str, cha);
+            Console.WriteLine(str + " contains " + cha + " " +num + " time(s).");
+            
+            
+            Console.ReadLine();
+            
+        }
+    }
+}
